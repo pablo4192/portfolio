885812248b9ac834af95e68ccdc123ac8bdd97b2
pablo4192 <estanguet570@gmail.com>
2023-04-04 21:53:50 -0300

    commit inicial

---
+++ entidades/clima.js
@@ -0,0 +1,45 @@
+export class Clima{
+    is_day;
+    gradosC;
+    gradosF;
+    humedad;
+    condicion;
+
+    constructor(is_day, gradosC, gradosF, humedad, condicion){
+        this.is_day = is_day;
+        this.gradosC = gradosC;
+        this.gradosF = gradosF;
+        this.humedad = humedad;
+        this.condicion = condicion;
+    }
+
+    retornarDatosFormateados(){
+        let retorno = new Clima();
+
+        retorno.is_day = this.is_day;
+        retorno.gradosC = `Grados Centigrados: ${this.gradosC}°C`;
+        retorno.gradosF = `Grados Farenheite: ${this.gradosF}°F`;
+        retorno.humedad = `Porcentaje de Humedad: ${this.humedad}%`;
+        retorno.condicion = this.traducirCondicion(this.condicion);
+         
+        return retorno;
+    }
+
+    traducirCondicion(condicion){
+        let despejado = ['sunny', 'clear'];
+        let nublado = ['overcast', 'cloudy'];
+        let lluvia = ['rain', 'showers', 'storm'];
+
+        if(condicion.toLowerCase().includes(despejado[0]) || condicion.toLowerCase().includes(despejado[1])){
+            return 'Soleado';
+        }
+        else if(condicion.toLowerCase().includes(nublado[0]) || condicion.toLowerCase().includes(nublado[1])){
+            return 'Nublado';
+        }
+        else if(condicion.toLowerCase().includes(lluvia[0]) || condicion.toLowerCase().includes(lluvia[1]) || condicion.toLowerCase().includes(lluvia[2])){
+            return 'Lluvioso';
+        }
+        return 'Indefinido';
+    }
+
+}
---
+++ index.js
@@ -0,0 +1,218 @@
+import { Clima }  from './entidades/clima.js';
+
+window.onload = () => {
+    abrirConsola();
+    cerrarConsola();
+    aplicarFx();
+    ocultarParrafoProyecto();
+    abrirModalClima();
+};
+
+const error_ref = document.getElementById('error');
+
+const abrirConsola = () => {
+    const a_ref = document.getElementById('consola');
+    const terminal_ref = document.getElementById('terminal');
+
+    a_ref.addEventListener('click', () => {
+        terminal_ref.style.display = 'block';
+    });
+}
+
+const cerrarConsola = () => {
+    const cerrar_ref = document.getElementById('cerrarConsola');
+    const terminal_ref = document.getElementById('terminal');
+
+
+    cerrar_ref.addEventListener('click', () => {
+        terminal_ref.style.display = 'none';
+    });
+}
+
+const aplicarFx = () => {
+    const underline_ref = document.getElementById('underline');
+    const imgPresentacion_ref = document.getElementById('imgMain2');
+    const presentacion_ref = document.getElementById('presentacion');
+    const opcionesTerm_ref = document.getElementsByClassName('opciones');
+    
+    window.addEventListener('scroll', () => {
+        let top = underline_ref.getBoundingClientRect().top;
+
+        if(top < 700){
+            underline_ref.style.width = '90%';
+        }
+        else if(top < 1400){
+            imgPresentacion_ref.style.opacity = '1';
+            presentacion_ref.style.opacity = '1';
+            presentacion_ref.style.transform = 'translate(0)';
+        }
+    });
+}
+
+const ocultarParrafoProyecto = () => {
+   
+    const proyectos_ref = document.getElementsByClassName('divProyecto');
+    
+    for(let i = 0; i < 4; i++){
+
+        proyectos_ref[i].addEventListener('mouseover', (e) => {
+            proyectos_ref[i].firstElementChild.style.display = 'none'; 
+        });
+
+        proyectos_ref[i].addEventListener('mouseleave', (e) => {
+            proyectos_ref[i].firstElementChild.style.display = 'flex';
+        });
+    }
+}
+
+const abrirModalClima = () => {
+    const opcion3_ref = document.getElementById('opcion3');
+    const modal_ref = document.getElementById('modal');
+    const cerrarModal_ref = document.getElementsByClassName('cerrarModal')[0].firstElementChild;
+    const consultar_ref = document.getElementsByClassName('consultaClima')[0].firstElementChild;
+    const ciudad_ref = document.getElementsByName('ciudad')[0];
+
+    opcion3_ref.addEventListener('click', () => {
+        modal_ref.style.display = 'flex';
+        modal_ref.showModal(); 
+    });
+
+    cerrarModal_ref.addEventListener('click', () => {
+        modal_ref.style.display = 'none';
+        modal_ref.close();
+    });
+
+    consultar_ref.addEventListener('click', () => {
+        
+
+        if(validarInputsClima(ciudad_ref.value)){
+            error_ref.style.visibility = 'hidden';
+            consultarClima(ciudad_ref.value);
+        }
+        else{
+            error_ref.style.visibility = 'visible';
+        }        
+    });
+    
+}
+
+function consultarClima(ciudad){
+    const options = {
+            method: 'GET',
+            headers: {
+            'X-RapidAPI-Key': '9b02cd9199msh4a4b6fd609b408fp1f3326jsnbd3421d41521',
+            'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+        }};
+    
+        fetch(`https://weatherapi-com.p.rapidapi.com/current.json?q=${ciudad}`, options)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+            error_ref.style.visibility = 'hidden';
+
+            let is_day = response.current.is_day;
+            let gradosC = response.current.temp_c;
+            let gradosF = response.current.temp_f;
+            let humedad = response.current.humidity;
+            let condicion = response.current.condition.text;
+            
+            let clima = new Clima(is_day, gradosC, gradosF, humedad, condicion);
+            let region = response.location.region;
+            let pais = response.location.country;
+            
+            insertarDatosClima(clima, ciudad, region, pais);
+        })
+        .catch(() => error_ref.style.visibility = 'visible'); 
+}
+
+const insertarDatosClima = (objClima, ciudad, region, pais) => {
+    const datosClima_ref = document.getElementById('datosClima');
+    let climaFormateado = objClima.retornarDatosFormateados();
+    let parrafo = document.createElement('p');
+    let texto = document.createTextNode(`Clima de hoy en ${ciudad} - ${region} - ${pais}`);
+
+    eliminarNodosHijos(datosClima_ref);
+    cambiarImg(climaFormateado);
+    datosClima_ref.style.display = 'flex';
+
+    parrafo.appendChild(texto);
+    datosClima_ref.appendChild(parrafo);
+    
+    for(let key in climaFormateado){
+
+        if(key != 'is_day'){
+            parrafo = document.createElement('p');
+            texto = document.createTextNode(climaFormateado[key]);
+        
+            parrafo.appendChild(texto);
+            datosClima_ref.appendChild(parrafo);
+        }
+    }
+}
+
+function validarInputsClima(ciudad){
+    let regExp = new RegExp('[a-zA-Z ]{2,20}');
+
+    return regExp.test(ciudad);
+}
+
+function cambiarImg(objClima){
+    const respuestaApi_ref = document.getElementById('respuesta');
+    const divImg = respuestaApi_ref.firstElementChild;
+    let img = document.createElement('img');
+
+    if(divImg.hasChildNodes()){
+        divImg.removeChild(divImg.firstElementChild);
+    }
+
+    if(objClima.is_day == 1){
+        respuestaApi_ref.classList.remove('respuestaApiNoche');
+        respuestaApi_ref.classList.add('respuestaApi');
+
+        if(objClima.condicion == 'Soleado'){
+            img.setAttribute('src', './assets/clima/soleado.png');
+            img.setAttribute('alt', 'soleado.png');
+        }
+    }
+    else if(objClima.is_day == 0){
+        respuestaApi_ref.classList.remove('respuestaApi');
+        respuestaApi_ref.classList.add('respuestaApiNoche');
+
+        if(objClima.condicion == 'Soleado'){
+            img.setAttribute('src', './assets/clima/luna.png');
+            img.setAttribute('alt', 'luna.png');
+        }
+    }
+
+    
+    switch(objClima.condicion){
+        case 'Nublado':
+            img.setAttribute('src', './assets/clima/nublado.png');
+            img.setAttribute('alt', 'nublado.png');
+                break;
+                case 'Lluvioso':
+                img.setAttribute('src', './assets/clima/lluvia.png');
+                img.setAttribute('alt', 'lluvia.png');
+                    break;
+    }
+
+    divImg.appendChild(img);
+}
+
+function eliminarNodosHijos(nodo){
+    let nodoHijo;
+    while(nodo.hasChildNodes()){
+        nodoHijo = nodo.removeChild(nodo.firstElementChild);
+    }
+}
+
+
+//Agregar efectos y estio al modal del clima
+//Realizar la opcion 2 de la consola (que puede ser??)
+//Agregar enlace al codigo del portfolio (opcion 2??)
+
+//Cambiar el estio de scroll al escoger seccion de la pagina (behavior..?)
+//Footer poner algo...
+
+
+
